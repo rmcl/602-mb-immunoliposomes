f85e6a3bfd254cfe437efca77cae8bf62c21e904
Russell McLoughlin <russ.mcl@gmail.com>
2018-10-21 13:17:10 -0700

    added static version of site.

---
+++ build/precache-manifest.85375e0826fac999e320615563d5bfa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c62265f9268785b9669",
+    "url": "/static/js/main.2c62265f.chunk.js"
+  },
+  {
+    "revision": "169fd073ecc1fa318244",
+    "url": "/static/js/1.169fd073.chunk.js"
+  },
+  {
+    "revision": "2c62265f9268785b9669",
+    "url": "/static/css/main.325b135a.chunk.css"
+  },
+  {
+    "revision": "585b1c54e76356d45fd3300f65669337",
+    "url": "/index.html"
+  }
+];
